c77a7ca51d4ba6c57796771a79aebf80807304fb
Your Name <zizoelfager14@gmail.com>
2023-12-29 02:43:39 +0200

    push

--- frontend/user_home/user_home_html.php
+++ frontend/user_home/user_home_html.php
@@ -11,6 +11,11 @@
     <script src="../../node_modules/jquery/dist/jquery.min.js"></script>
     <script src="../..node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
  
+
+ <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/col_navbar.css">
+    <link rel="stylesheet" href="../css/user_profile.css">
+
  <?php
             $filename = '../../backend/user_home/user_home.php';
             if (file_exists($filename)) {
@@ -22,25 +27,58 @@
     <!-- js file-->
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script type="text/javascript"  src="./js/user_home.js"></script>
+
+        <link rel='stylesheet' href='../../node_modules/@fortawesome/fontawesome-free/css/all.min.css'>
+    <link rel="stylesheet" href="../../frontend/nav_jss/navbar-pure-css/dist/style.css">
 </head>
 <body>
-    <div class="header">
-    <div class="container">
-        <div class="logo">LOGO ssssssssssIMAGE</div>
+  <div class="flex-container">
+        <!-- Your nav-bar code remains unchanged -->
+        <div id="nav-bar">
+            <input id="nav-toggle" type="checkbox" />
+            <div id="nav-header"><a id="nav-title" target="_blank"><i></i>Profile</a>
+                <label for="nav-toggle"><span id="nav-toggle-burger"></span></label>
+                <hr />
+            </div>
+            <div id="nav-content">
+                <div class="nav-button"><i class="fas fa-home"></i><span><a
+                            href="../user_home/user_home_html.php">Home</a></span></div>
+                <div class="nav-button"><i class="fas fa-user"></i><span><a
+                            href="../user_edit_profile/userEditProfilehtml.php">Edit profile</a></span></div>
+                <div class="nav-button"><i class="fas fa-thumbtack"></i><span><a href="../../backend/logout/logout.php">
+                            log out</a></span></div>
+                <hr />
 
-        <div class="links">
-            <ul class="navbar">
-                <li><a href="homepage.html" >Home</a></li> <!--3alaa-->
-                <li><a href="../user_profile/profilehtml.php">Profile</a></li>
-            </ul>
+                <div class="nav-button"><i class="fas fa-key"></i><span><a
+                            href="../../frontend/change_customer_password/change_customer_password_html.php">Change
+                            Password</a></span></div>
+                <div class="nav-button"><i class="fas fa-car"></i><span><a
+                            href="../../frontend/show_all_customer_reservation/show_all_customer_reservation.php">My
+                            reservation</a></span></div>
+
+
+
+                <div id="nav-content-highlight"></div>
+            </div>
+            <input id="nav-footer-toggle" type="checkbox" />
+            <div id="nav-footer">
+                <div id="nav-footer-heading">
+                    <div id="nav-footer-avatar"><img
+                            src="https://gravatar.com/avatar/4474ca42d303761c2901fa819c4f2547" /></div>
+                    <div id="nav-footer-titlebox"><a id="nav-footer-title" href="https://codepen.io/uahnbu/pens/public"
+                            target="_blank">uahnbu</a><span id="nav-footer-subtitle">Admin</span></div>
+                    <label for="nav-footer-toggle"><i class="fas fa-caret-up"></i></label>
+                </div>
+                <div id="nav-footer-content">
+                    <Lorem>ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                        et
+                        dolore magna aliqua.</Lorem>
+                </div>
+            </div>
         </div>
-        <div class="header-btn">
-            <a href="logout.php" class="sign-in">Logout</a>
-        </div>
-    </div>
-    </div>
 
   
+        <div class="content-of-page">
 
     <!--do all select-->
     <div class="container mt-4"> 
@@ -77,6 +115,8 @@
     <div id="reciveddata">
     </div>
 
+    </div>
+    </div>
 
     </div>
 </body>
